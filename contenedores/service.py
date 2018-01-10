7c413af0454f1595c5001537aeefa9f39c8aea9a
AGM-GR <alejandroguerrero.gr@gmail.com>
2018-01-10 01:30:39 +0100

    Actualizado script servicio.py #7

--- contenedores/service.py
+++ contenedores/service.py
@@ -4,13 +4,18 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+class Home(Resource):
+    def get(self):
+        return jsonify({'status': 'ok'})
+
 class Status(Resource):
     def get(self):
         return jsonify({'status': 'ok'})
 
 
+api.add_resource(Home, '/')
 api.add_resource(Status, '/status')
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, host='0.0.0.0')
